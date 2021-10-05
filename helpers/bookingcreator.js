e1daa2e13424f9cf06f24cb0dea88e832396a8b8
azeljkovic <aleksandar.zeljkovic91@gmail.com>
2021-10-05 22:15:32 +0200

    commented redundant code

--- helpers/bookingcreator.js
+++ helpers/bookingcreator.js
@@ -5,46 +5,45 @@ var dateFormat = require('dateformat');
 var counter = require("../models/booking");
 const json = require("./bookings.json");
 
-var randomiseDate = function(start, end) {
-    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
-};
-
-var randomiseNumber = function(from, to){
-    return Math.floor(Math.random() * (to - from + 1) + from);
-};
-
-var randomiseFirstName = function(){
-  var name = ["Mark","Mary","Sally","Jim","Eric","Susan"]
-
-  return name[randomiseNumber(0,name.length - 1)];  
-};
-
-var randomiseLastName = function(){
-  var surname = ["Jones","Wilson","Jackson","Brown","Smith","Ericsson"]
-
-  return surname[randomiseNumber(0,surname.length - 1)];  
-};
-
-var randomiseBool = function(){
-  var bool = [true,false]
-
-  return bool[randomiseNumber(0,bool.length - 1)];
-};
+// var randomiseDate = function(start, end) {
+//     return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+// };
+//
+// var randomiseNumber = function(from, to){
+//     return Math.floor(Math.random() * (to - from + 1) + from);
+// };
+//
+// var randomiseFirstName = function(){
+//   var name = ["Mark","Mary","Sally","Jim","Eric","Susan"]
+//
+//   return name[randomiseNumber(0,name.length - 1)];
+// };
+//
+// var randomiseLastName = function(){
+//   var surname = ["Jones","Wilson","Jackson","Brown","Smith","Ericsson"]
+//
+//   return surname[randomiseNumber(0,surname.length - 1)];
+// };
+//
+// var randomiseBool = function(){
+//   var bool = [true,false]
+//
+//   return bool[randomiseNumber(0,bool.length - 1)];
+// };
+//
+// if(randomiseBool()){
+//   booking.additionalneeds = "Breakfast";
+// }
 
 exports.createBooking = function(){
-  var checkInDate = randomiseDate(new Date(2015, 1, 1), new Date())
-  var latestDate = new Date()
-  latestDate.setDate(latestDate.getDate() + 3)
+  // var checkInDate = randomiseDate(new Date(2015, 1, 1), new Date())
+  // var latestDate = new Date()
+  // latestDate.setDate(latestDate.getDate() + 3)
 
   // static data seed modification
   const json = require('./bookings.json');
   let booking = json[counter.getCounter()];
   // ******************************
-
-
-  if(randomiseBool()){
-    booking.additionalneeds = "Breakfast";
-  }
 
   return booking;
 }
